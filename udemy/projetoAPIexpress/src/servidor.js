165c690bce309f9eb9b515ffdd5d45fac1e0472a
willianctti <willianctti@gmail.com>
2023-03-08 10:04:43 -0300

    finalizando api com banco de dados usando postman CRUD

--- udemy/projetoAPIexpress/src/servidor.js
+++ udemy/projetoAPIexpress/src/servidor.js
@@ -1,11 +1,46 @@
 const porta = 3003
 
 const express = require('express')
-const app = express()
+const app = express().use(express.json())
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: "Notebook", preco: 1232.32 }) // converter para JSON automaticamente
-} )
+   res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
+})
 
 app.listen(porta, () => {
     console.log('Rodando')
